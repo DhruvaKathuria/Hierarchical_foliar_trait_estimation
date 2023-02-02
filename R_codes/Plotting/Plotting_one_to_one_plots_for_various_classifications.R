a73c97125aa1518fc0ad0f8e004a586dd6321e29
Dhruva Kathuria <kathuria.dhruva@gmail.com>
2023-02-02 11:49:18 -0500

    Updated ECOSIS_implementation_for_Bayesian_ML.R and added an R code to plot the one-to-one plots for predictions/observations for various classifications
    The ECOSIS_implementation_for_Bayesian_ML.R now outputs a data frame which has the predictions as well as all the metadata for a particular trait. This code is called inside the R code "Plotting_one_to_one_plots_for_various_classifications.R" which then plots the resulting predictions with the observations for various metadata classifications.

---
+++ R_codes/Plotting/Plotting_one_to_one_plots_for_various_classifications.R
@@ -0,0 +1,83 @@
+library(cowplot)
+rm(list = ls())
+mainDir = "/Users/dhruvakathuria/Library/Mobile Documents/com~apple~CloudDocs/NASA_work/NASA_proposal_3.1.2/ECOSIS_Data_download_Dhruva"  
+Github_dir = "/Users/dhruvakathuria/Documents/GitHub/Hierarchical_foliar_trait_estimation/"
+
+###########Parameters to change in the code###################
+algorithm1 = "ridge" # can be PLSR, "Bayesian_linear_horseshoe; check "Apply_ML_and_prospect_algorithms.R" for various options on algorithms
+filtering_type = "Site_specific" # "Global", "Site_specific"
+trait_name1 = "Carotenoid_Area"
+
+# The below code gives "data_mat_test" which gives us the test matrix for a trait with predictions as "pred" and observations as "trait"
+source(paste0(Github_dir, "R_codes/Regression_algorithms/ECOSIS_Implementation_file_for_Bayesian_ML.R"))
+
+###################################################Functions#####################################################
+
+function_for_plotting <- function(data_mat, name1) # name1 is either "Global" or "a dataset name out of the datsets"
+{
+  gg_pred <- gg_error <- list()  # these are the ggplot elements for each of the metadata_vector, one is for plotting the predicitons, and the other is for plotting the errors
+  for(jj in 1: 3)
+  {
+    metadata_filter = metadata_vector[jj]
+    
+    data_mat = data_mat[!data_mat[, metadata_filter] %in% c("Data unavailable", "Error in extraction, check"), ]
+    data_mat$trait = as.numeric(data_mat$trait)
+    data_mat$error = (data_mat$pred - data_mat$trait)
+    
+    gg_pred[[metadata_filter]] <-  ggplot(data_mat, aes(x = trait, y = pred, color = .data[[metadata_filter]])) +
+      geom_point() + geom_abline()+ ggtitle(name1) + scale_fill_manual(values = colors_list[[jj]]) +
+      theme(plot.title = element_text(size = 6))
+    
+    gg_error[[metadata_filter]] <- ggplot(data_mat, aes(x = trait, y = error, color = .data[[metadata_filter]])) +
+      geom_point() + ggtitle(name1) +scale_fill_manual(values = colors_list[[jj]]) +
+      theme(plot.title = element_text(size = 6)) #+ geom_abline()
+  }
+  list(pred = gg_pred, error = gg_error)
+}
+#################################################################################################################
+
+##########################################Making plots for the predictions and observations#########################
+
+metadata_vector = c("Growth_form", "Leaf", "Phenology") # this is from get_growth_form_phenology_leaf_type_etc_from_Wiki.R
+
+######################################Color_lists#########################################
+colors_list <- list()
+colors_list[[1]] <- c("tree" = "#FF0000", "grass" = "#00FF00", "herbaceous" = "#0000FF",
+                      "vine" = "#FFFF00", "shrub" = "#00FFFF")
+colors_list[[2]] <- c("broad" = "#FF0000", "needle" = "#00FFFF")
+colors_list[[3]] <- c("deciduous" = "#FF0000", "evergreen" = "#00FFFF")
+############################################################################################
+
+
+if(filtering_type == "Global")
+{
+  out_plots = function_for_plotting(data_mat_test, "Global")
+}else if(filtering_type == "Site_specific")
+{
+  out_plots = mapply(function_for_plotting, data_mat_test, datasets_to_take_for_trait, SIMPLIFY = F)
+}
+
+###Example for combined plot for Site_specific
+if(filtering_type == "Global")
+{
+  for(metadata_filter in metadata_vector)
+  {
+    out_plots[["pred"]]
+    out_plots[["error"]]
+  }
+}
+
+
+if(filtering_type == "Site_specific")
+{
+  for(metadata_filter in metadata_vector)
+  {
+    plot_grid(out_plots[[datasets_to_take_for_trait[1]]] [["pred"]] [[metadata_filter]], out_plots[[datasets_to_take_for_trait[2]]] [["pred"]] [[metadata_filter]], out_plots[[datasets_to_take_for_trait[3]]] [["pred"]] [[metadata_filter]],
+              out_plots[[datasets_to_take_for_trait[4]]] [["pred"]] [[metadata_filter]], out_plots[[datasets_to_take_for_trait[5]]] [["pred"]] [[metadata_filter]], out_plots[[datasets_to_take_for_trait[6]]] [["pred"]] [[metadata_filter]])
+    
+    plot_grid(out_plots[[datasets_to_take_for_trait[1]]] [["error"]] [[metadata_filter]], out_plots[[datasets_to_take_for_trait[2]]] [["error"]] [[metadata_filter]], out_plots[[datasets_to_take_for_trait[3]]] [["error"]] [[metadata_filter]],
+              out_plots[[datasets_to_take_for_trait[4]]] [["error"]] [[metadata_filter]], out_plots[[datasets_to_take_for_trait[5]]] [["error"]] [[metadata_filter]], out_plots[[datasets_to_take_for_trait[6]]] [["error"]] [[metadata_filter]])
+  
+  }
+}
+##########################################################################################################
--- R_codes/Regression_algorithms/ECOSIS_Implementation_file_for_Bayesian_ML.R
+++ R_codes/Regression_algorithms/ECOSIS_Implementation_file_for_Bayesian_ML.R
@@ -1,65 +1,22 @@
 library(dplyr)
 library(arrow)
 library(units)
+library(ggplot2)
 
-mainDir = "/Users/dhruvakathuria/Library/Mobile Documents/com~apple~CloudDocs/NASA_work/NASA_proposal_3.1.2/ECOSIS_Data_download_Dhruva"  
-Github_dir = "/Users/dhruvakathuria/Documents/GitHub/Hierarchical_foliar_trait_estimation/"
+source("/Users/dhruvakathuria/Documents/GitHub/Hierarchical_foliar_trait_estimation/R_codes/Regression_algorithms/Apply_ML_and_prospect_algorithms.R")
+#################################################functions used in this code#######################################
 
-trait_name1 = "Carotenoid_Area"
-datasets_already_processed =  list.files(mainDir, recursive = T, pattern = "traits_already_done_for_metadata.txt")
-indices_of_datasets_containing_trait_name =  unlist(lapply(datasets_already_processed, function(x)
-{
-  read_file1 = readr :: read_lines(file.path(mainDir, x))
-  trait_name1 %in% read_file1
-})
-)
-
-datasets_to_take_for_trait = unlist(lapply(datasets_already_processed[indices_of_datasets_containing_trait_name], function(x) strsplit(x, "/traits_already_done_for_metadata.txt" )[[1]])) 
-
-#################################################################################################################
-## The below is just a quick fix (Just for Caretonoid) for a first pass at data. Will be replaced by a more formal approach for dealing with spectra
-# fixing the spectra range based on seeing what is the extent of spectra variation in the data
-# Uncomment the for loop if you want to see what colnames we have for spectra
-
-# for(i in 1:length(datasets_to_take_for_trait))
-# {
-#   spectra = readr :: read_csv(file = paste0(file.path(mainDir, datasets_to_take_for_trait[i]), "/", "spectra.csv"), show_col_types = FALSE)
-#   print(datasets_to_take_for_trait[i])
-#   print(colnames(spectra)[1])
-#   print(colnames(spectra)[ncol(spectra)])
-#   print(ncol(spectra))
-#   print(as.numeric(colnames(spectra)[ncol(spectra)]) - as.numeric(colnames(spectra)[1]))
-#   print(which(is.na(spectra)))
-# }
-
-
-filter_spectra_data = function(x)
+filter_spectra_data = function(x) # this function only works for Carotenoid data for filtering the spectra # need to solve the Github issue
 {
   spectra = readr :: read_csv(file = paste0(file.path(mainDir, x), "/", "spectra.csv"), show_col_types = FALSE)
   index_start = which(as.numeric(colnames(spectra)) == 400 )
   index_end = which(as.numeric(colnames(spectra)) == 2400)
   
   spectra_filtered = spectra[, index_start:index_end]
-
+  
 }
 
-spectra_filtered = lapply(datasets_to_take_for_trait, filter_spectra_data)
-
-# Putting all the spectra_filtered list into a matrix form
-for(i in 1:length(spectra_filtered))
-{
-  if(i == 1)
-  {
-    spectra_matrix = spectra_filtered[[i]]
-  }else{spectra_matrix = rbind(spectra_matrix, spectra_filtered[[i]])}
-}
-
-#################################################################################################################
-
-##### Getting traits together
-
-
-filter_trait_data_and_metadata = function(x, trait_name1)
+filter_trait_data_and_metadata = function(x, trait_name1) # attaches the trait value and also the metadata associated with species as well as the instrument error
 {
   metadata_arrow_version = read_parquet(paste0(file.path(mainDir, x), "/", "metadata_updated.parquet"))
   name_for_trait = attributes(metadata_arrow_version)$trait_names[[trait_name1]][["trait_name"]]
@@ -80,74 +37,84 @@ filter_trait_data_and_metadata = function(x, trait_name1)
     trait_value = set_units(trait_value, microgram/cm^2)
   }
   
-  metadata_merged = merge(metadata_arrow_version, input_database, by.x = "genus_species1" , by.y = "Scientific_name", all.x = TRUE)
+  metadata_merged = merge(metadata_arrow_version, database_for_metadata, by.x = "genus_species1" , by.y = "Scientific_name", all.x = TRUE)
   metadata_merged = metadata_merged %>% select(genus_species1, family1, Growth_form, Phenology, Leaf, manufacturer, model)
   metadata_merged$trait = trait_value
   metadata_merged$site_name = rep(x, nrow(metadata_merged))
   metadata_merged
 }
 
-trait_and_metadata_dataframe_list = lapply(datasets_to_take_for_trait, filter_trait_data_and_metadata, trait_name1 =  "Carotenoid_Area")
-## IMP: Need to have a check which confirms that the units of each list are the same
-
-trait_and_metadata_dataframe = do.call(rbind, trait_and_metadata_dataframe_list)
-
-# function to remove rows based on a metadata
-filter_na_values <- function(data, column) 
-{
-  data_filtered <- data[!is.na(data[, column]), ] # Filter the data frame based on the presence of NA values in the specified column
-  return(data_filtered) # Return the filtered data frame
-}
-
-## Dividing into test and train data
-
-get_indices_subset_function <- function(data_frame1, filtering_type, test_site, fraction_split)
+get_indices_subset_function <- function(data_frame1, filtering_type, test_site, fraction_split) # get the indices for the training and testing data according to whether the split is to made globally or site-wise
 {
   if(filtering_type == "Global")
   {
     indices_subset = sample(1:nrow(data_frame1), fraction_split * nrow(data_frame1))
   }else if(filtering_type == "Site_specific")
   {
-   indices_subset = which(data_frame1$site_name != test_site)
+    indices_subset = which(data_frame1$site_name != test_site)
   }
   indices_subset
 }
 
-indices_subset= get_indices_subset_function(trait_and_metadata_dataframe, filtering_type = "Global", test_site = "None", fraction_split = 0.7)
-indices_subset_local = get_indices_subset_function(trait_and_metadata_dataframe, filtering_type = "Site_specific", test_site = datasets_to_take_for_trait[1]  , fraction_split = 0.7)
+get_test_data_frame_predictions = function(indices_subset, algorithm1) # gives the test dataframe along with the predictions based on algorithm1. The data frame also contains the metadata useful for further exploratory analysis
+{
+  Y_train = as.numeric(trait_and_metadata_dataframe$trait[indices_subset ])
+  Y_test = as.numeric(trait_and_metadata_dataframe$trait[-indices_subset ])
+  X_train = spectra_df[indices_subset, ]
+  X_test = spectra_df[-indices_subset, ]
+  Regression_apply2 = apply_regression_algorithm2 (algorithm1, X_train, Y_train, X_test, Y_test)
+  data_mat_out = trait_and_metadata_dataframe[-indices_subset, ]
+  data_mat_out$pred = Regression_apply2$predictions
+  data_mat_out
+}
 
-###########################################################################################################
-source("/Users/dhruvakathuria/Documents/GitHub/Hierarchical_foliar_trait_estimation/R_codes/Regression_algorithms/Apply_ML_and_prospect_algorithms.R")
 
-# prepping data
-Y_train = as.numeric(trait_and_metadata_dataframe$trait[indices_subset ])
-Y_test = as.numeric(trait_and_metadata_dataframe$trait[-indices_subset ])
-X_train = spectra_matrix[indices_subset, ]
-X_test = spectra_matrix[-indices_subset, ]
+###############################################Datasets########################################################################
+datasets_already_processed =  list.files(mainDir, recursive = T, pattern = "traits_already_done_for_metadata.txt")
+database_for_metadata = readr :: read_csv(paste0(Github_dir, "R_codes/Species_data/Species_attribute_data_Dhruva.csv")) # this is the metadata file that I made from "get_growth_form_phenology_leaf_type_etc_from_Wiki.R"datasets_already_processed =  list.files(mainDir, recursive = T, pattern = "traits_already_done_for_metadata.txt") # we take the datasets for which atleast one of the traits has been processed
+indices_of_datasets_containing_trait_name =  unlist(lapply(datasets_already_processed, function(x)
+{
+  read_file1 = readr :: read_lines(file.path(mainDir, x))
+  trait_name1 %in% read_file1
+})
+)
 
-PLSR_values = apply_regression_algorithm2 ("ridge", X_train, Y_train, X_test, Y_test)
-plot(PLSR_values$obs,PLSR_values$predictions)
-abline(0, 1, col = "red")
+datasets_to_take_for_trait = unlist(lapply(datasets_already_processed[indices_of_datasets_containing_trait_name], function(x) strsplit(x, "/traits_already_done_for_metadata.txt" )[[1]])) 
 
-ridge_values = apply_regression_algorithm2 ("ridge", spectra_matrix[indices_subset, ], Carotenoid_vector[indices_subset ], spectra_matrix[-indices_subset, ], Carotenoid_vector[-indices_subset])
-plot(ridge_values$obs,ridge_values$predictions)
-abline(0, 1, col = "red")
+spectra_filtered = lapply(datasets_to_take_for_trait, filter_spectra_data)
+spectra_df = do.call(rbind, spectra_filtered)
 
-Bayesian_values2 = apply_regression_algorithm2 ("Bayesian_linear_horseshoe", spectra_matrix[indices_subset, ], Carotenoid_vector[indices_subset ], spectra_matrix[-indices_subset, ], Carotenoid_vector[-indices_subset])
-plot(Bayesian_values2$obs, Bayesian_values2$predictions)
-abline(0, 1, col = "red")
-############################################################################################################
+trait_and_metadata_dataframe_list = lapply(datasets_to_take_for_trait, filter_trait_data_and_metadata, trait_name1 =  "Carotenoid_Area")
+## IMP: Need to have a check which confirms that the units of each list are the same
+trait_and_metadata_dataframe = do.call(rbind, trait_and_metadata_dataframe_list)
+#################################################################################################################
 
-library(ggplot2)
-metadata_filter = "Growth_form"
+## The below is just a quick fix (Just for Caretonoid) for a first pass at data. Will be replaced by a more formal approach for dealing with spectra
+# fixing the spectra range based on seeing what is the extent of spectra variation in the data
+# Uncomment the for loop if you want to see what colnames we have for spectra
 
-data_mat = trait_and_metadata_dataframe[-indices_subset, ]
-Regression_apply2 = apply_regression_algorithm2 ("PLSR", X_train, Y_train, X_test, Y_test)
-data_mat$pred = Regression_apply2$predictions
-data_mat = data_mat[!data_mat[, metadata_filter] %in% c("Data unavailable", "Error in extraction, check"), ]
-data_mat$trait = as.numeric(data_mat$trait)
-data_mat$error = (data_mat$pred - data_mat$trait)
+# for(i in 1:length(datasets_to_take_for_trait))
+# {
+#   spectra = readr :: read_csv(file = paste0(file.path(mainDir, datasets_to_take_for_trait[i]), "/", "spectra.csv"), show_col_types = FALSE)
+#   print(datasets_to_take_for_trait[i])
+#   print(colnames(spectra)[1])
+#   print(colnames(spectra)[ncol(spectra)])
+#   print(ncol(spectra))
+#   print(as.numeric(colnames(spectra)[ncol(spectra)]) - as.numeric(colnames(spectra)[1]))
+#   print(which(is.na(spectra)))
+# }
 
-ggplot(data_mat, aes(x = trait, y = error, color = .data[[metadata_filter]])) +
-  geom_point() #+ geom_abline()
+
+
+if(filtering_type == "Global")
+{
+  indices_subset = get_indices_subset_function(trait_and_metadata_dataframe, filtering_type = "Global", test_site = "None", fraction_split = 0.7)
+  data_mat_test =   get_test_data_frame_predictions(indices_subset, algorithm1)
+}else if (filtering_type == "Site_specific") 
+{
+  indices_subset = lapply(datasets_to_take_for_trait, get_indices_subset_function, filtering_type = "Site_specific", data_frame1 = trait_and_metadata_dataframe, fraction_split = 0.7)
+  data_mat_test = lapply(indices_subset, get_test_data_frame_predictions, algorithm1 = "ridge") 
+  names(data_mat_test) = datasets_to_take_for_trait
+}
+
 
