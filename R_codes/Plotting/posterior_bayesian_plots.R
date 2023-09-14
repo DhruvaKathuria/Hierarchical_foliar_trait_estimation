af9e968ebda490f48b57c76fe9caf76d856b6714
Dhruva Kathuria <kathuria.dhruva@gmail.com>
2023-09-14 15:01:45 -0400

    updated paper

---
+++ R_codes/Plotting/posterior_bayesian_plots.R
@@ -0,0 +1,7 @@
+# Setting global parameters -----------------------------------------------
+source("R_codes/input_parameter_file.R")
+
+
+# posterior predictive checks plots ---------------------------------------
+
+
