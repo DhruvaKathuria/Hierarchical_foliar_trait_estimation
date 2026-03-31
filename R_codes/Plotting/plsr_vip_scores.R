3dec3dc0441aa283dafe458ffba8716d8226b2f9
Dhruva Kathuria <kathuria.dhruva@gmail.com>
2026-03-31 15:09:21 -0400

    vip_scores

---
+++ R_codes/Plotting/plsr_vip_scores.R
@@ -0,0 +1,45 @@
+#plotting plsr coefficients and vip scores
+library(ggplot2)
+library(patchwork)
+# Data
+trait_name1 = "LMA"
+color_vector = c("Carotenoid_Area" =  "#F8766D", 
+                 "LMA" =   "#00BA38", 
+                 "Nitrogen" =  "#619CFF")
+data_folder = "/Users/dkathuri/Downloads/Github_data/Hierarchical_foliar_trait_estimation"
+
+trait_names_for_plots  = c("LMA" = "LMA",
+                           "Carotenoid_Area" = "Carotenoid",
+                           "Nitrogen" = "Nitrogen")
+plsr_df = read_csv(file.path(data_folder, "data", "code_output_data", "plsr_files", "plsr_regression_coefs_plus_vip_scores.csv"))
+
+plsr_vip_scores = list()
+
+for(trait_name1 in c("Carotenoid_Area", "LMA", "Nitrogen"))
+{
+
+  plsr_vip_scores[[trait_name1]] <- plsr_df |> 
+    filter(trait == trait_names_for_plots[trait_name1]) |> 
+    ggplot(aes(x = wavelengths, y = vip)) +
+    geom_line(color = color_vector[trait_name1]) +
+    geom_hline(yintercept = 1, linetype = "dashed", color = "black") +
+    scale_x_continuous(
+      limits = c(400, 2400),
+      breaks = seq(400, 2400, by = 200)  # optional, cleaner ticks
+    ) +
+    theme(axis.text.x = element_text(angle = 90, hjust = 1),
+          axis.title.y = element_blank(),
+          axis.ticks.y = element_blank(),
+          strip.text = element_blank(),
+          legend.position = "none") +
+    xlab("")
+  
+}
+plsr_vip_scores_comp_plot <- plsr_vip_scores[[1]] / plsr_vip_scores[[2]] /plsr_vip_scores[[3]]
+plsr_vip_scores_comp_plot
+
+ggsave(filename = "paper_draft/figures/plsr_vip_scores.png",
+       plsr_vip_scores_comp_plot,
+       width = 7.5,
+       height = 10,
+       units = "in")
--- R_codes/Regression_algorithms/plsr_coefficients_and_vip_scores.R
+++ R_codes/Regression_algorithms/plsr_coefficients_and_vip_scores.R
@@ -5,11 +5,12 @@
 library(pls)
 library(ggplot2)
 library(tidyverse)
+library(plsVarSel)
 
 data_folder = "/Users/dkathuri/Downloads/Github_data/Hierarchical_foliar_trait_estimation"
 
 # Data
-trait_name1 = "LMA"
+#trait_name1 = "LMA"
 color_vector = c("Carotenoid_Area" =  "#F8766D", 
                  "LMA" =   "#00BA38", 
                  "Nitrogen" =  "#619CFF")
@@ -22,7 +23,7 @@ trait_names_for_plots  = c("LMA" = "LMA",
 get_plsr_regression_coefs_and_vip_scores = function(trait_name1)
   
 {
-  pls_object = readRDS(file.path(data_folder, "data", "code_output_data", str_glue("{trait_name1}_LOO_PLSR_object_March_2026.rds")))
+  pls_object = readRDS(file.path(data_folder, "data", "code_output_data", "plsr_files", str_glue("{trait_name1}_LOO_PLSR_object_March_2026.rds")))
   RMSE_values_cv = RMSEP(pls_object)
   RMSE_values = RMSE_values_cv$val[1, ,]
   index1_min = which.min(RMSE_values[1:100])
@@ -30,7 +31,7 @@ get_plsr_regression_coefs_and_vip_scores = function(trait_name1)
   #plotting plsr regression coefficients
   
   coef_vec <- as.vector(coef(pls_object, ncomp = index1_min))
-  vip_scores = VIP(ll_1, opt.comp = index1_min)
+  vip_scores = VIP(pls_object, opt.comp = index1_min)
   wavelengths = 400:2400
   
   plsr_df = data.frame(wavelengths = wavelengths,
